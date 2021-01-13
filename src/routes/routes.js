47cf00359c9c918c2d5e77814538b3ee5d089fab
Ryan JT <ryantalbot7@gmail.com>
2021-01-13 13:16:14 -0600

    Add finalized section 4

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,19 @@
-import { addnewProduct } from '../controllers/Controllers';
+import {
+	addnewProduct,
+	getProducts,
+	getProductWithID,
+} from '../controllers/Controllers';
 
 const routes = (app) => {
 	app.route('/products');
 
 	// Post endpoint
 	app.post(addnewProduct);
+
+	app.get(getProducts);
+
+	app.route('/products/:ProductID');
+	app.get(getProductWithID);
 };
 
 export default routes;
